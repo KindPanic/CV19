19000c3c6ae6dd2b3d7093f2eb112b5e0af52c30
Roman Semykin <KindPanic@mail.ru>
2020-08-25 22:06:03 +0300

    Структура + команды

---
+++ CV19/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,27 @@
+﻿using CV19.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CV19.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+        private string _Title = "Анализ статистики CV19";
+        /// <summary>Заголовок окна</summary>
+        public string Title
+        {
+            get => _Title;
+            set => Set(ref _Title, value);
+        }
+
+        private string _Status = "Готов!";
+        /// <summary>Статус бар</summary>
+        public string Status
+        {
+            get => _Status;
+            set => Set(ref _Status, value);
+        }
+
+    }
+}
